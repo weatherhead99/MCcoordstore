7b5b83f871a26a546f459405ba5e61c2e74c3102
Dan <dan.weatherill@cantab.net>
2021-12-11 20:25:55 +0000

    ready for pyPI initial upload?

--- setup.py
+++ setup.py
@@ -1,18 +1,18 @@
-     MCcoordstore - simple web based store for Minecraft points of interest
-     Copyright (C) 2021  Daniel Philip Weatherill
+#      MCcoordstore - simple web based store for Minecraft points of interest
+#      Copyright (C) 2021  Daniel Philip Weatherill
 
-     This program is free software: you can redistribute it and/or modify it
-under the terms of the GNU Affero General Public License as published by the
-Free Software Foundation, either version 3 of the License, or (at your option)
-any later version.
+#      This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU Affero General Public License as published by the
+# Free Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
 
-     This program is distributed in the hope that it will be useful, but WITHOUT
-ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
-FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
-details.
+#      This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE.  See the GNU Affero General Public License for more
+# details.
 
-     You should have received a copy of the GNU Affero General Public License
-along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#      You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 
 from setuptools import setup, find_packages
@@ -23,8 +23,15 @@ setup(name="MCcoordstore",
       license="AGPL-3.0-or-later",
       author="Dan Weatherill",
       author_email="dan.weatherill@cantab.net",
+      url="https://github.com/weatherhead99/MCcoordstore",
       packages = find_packages(),
       include_package_data = True,
+      classifiers = [
+          "Development Status :: 2 - Pre-Alpha",
+          "Framework :: Flask",
+          "License :: OSI Approved :: GNU Affero General Public License v3",
+          "Topic :: Internet :: WWW/HTTP :: WSGI :: Application"
+          ],
       install_requires = ["flask",
                           "flask_sqlalchemy",
                           "Flask-WTF",
