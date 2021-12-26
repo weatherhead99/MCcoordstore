35122faf5c521dbf5d9c48a7853394639f926fa3
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2021-12-26 23:50:10 +0000

    working single SVG render

--- MCcoordstore/static/scripts/plotly_render.js
+++ MCcoordstore/static/scripts/plotly_render.js
@@ -1,7 +1,8 @@
+const SVGNS = "http://www.w3.org/2000/svg";
+
 class PlotlyShapeRenderer {
     constructor() {
 	this._plotdom = document.createElement('div');
-	this._renderdom = document.createElement('svg');
 
 	Plotly.newPlot(this._plotdom, [{x : [0], y : [0]}]);
 	this._n_items = 0;
@@ -24,21 +25,50 @@ class PlotlyShapeRenderer {
 	
     }
 
-    render_svg() {
-	const svgitems = this._plotdom.querySelectorAll("path.point");
-	console.log("n svgitems")
-	console.log(svgitems.length);
-	var paths = new Array();
-	for(const [k,v] of Object.entries(svgitems))
-	    paths.push(v.attributes.d.value);
-	
+    render_one_svg(idx) {
+	const item_in = this._plotdom.querySelectorAll("path.point")[idx];
+	const path = item_in.attributes.d.value;
+	const svgitem = document.createElementNS(SVGNS, "path");
+	svgitem.setAttributeNS(null, "class", "rendersym");
+	svgitem.setAttributeNS(null, "d", path);
+
+	return svgitem;
     }
 
     get plotdom() { return this._plotdom;}
+    get renderdom() { return this._renderdom;}
+};
 
+function svg_centred_render_helper(pathitem, target,  width, applyscale=null)
+{
+    const item = document.createElementNS(SVGNS, "svg");
+
+    let translatestr = "";
+    if(applyscale != null)
+    {
+	translatestr +=  "scale(" + applyscale + ")";
+    }
+
+    const attacheditem = item.appendChild(pathitem);
+    target.appendChild(item);
+
+    
+    if(width < 0)
+    {
+	item.setAttributeNS(null,"width", 10000);
+	item.setAttributeNS(null,"height", 10000);
+	const bbox = attacheditem.getBBox();
+	item.setAttributeNS(null,"width", bbox.width);
+	item.setAttributeNS(null,"height",bbox.height);
+	width = bbox.width;
+    }
+    else
+    {
+	item.setAttributeNS(null, "width", width);
+	item.setAttributeNS(null, "height", width);
+    }
+
+    translatestr += " translate(" + width/2 + "," + width/2 + ")";
+
+    attacheditem.setAttributeNS(null, "transform",translatestr);
 }
-
-function test () {
-    console.log("test");
-}
-
